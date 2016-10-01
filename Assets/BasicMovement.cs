b31615349183321098010604d7618b4918ec5fdb
Girish Raman <rgirish1994@gmail.com>
2016-09-30 17:35:57 -0700

    Added rotation wrt the mouse direction

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class BasicMovement : MonoBehaviour {
 
     public float speed = 10.0f;
+    public float rotationSpeed = 2.0f;
 
 	void Update () {
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        gameObject.GetComponent<CharacterController>().Move(input * speed * Time.deltaTime);
+        gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input * speed * Time.deltaTime));
+        transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
 	}
 }
