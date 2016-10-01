976a46bb6e1d02932e8655f4a22c59062ff32bef
Girish Raman <rgirish1994@gmail.com>
2016-09-30 17:14:03 -0700

    First commit
    Capsule moves in XZ plane, collides (and stops) with cubes.

---
+++ Assets/BasicMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        gameObject.GetComponent<CharacterController>().Move(input * speed * Time.deltaTime);
+	}
+}
